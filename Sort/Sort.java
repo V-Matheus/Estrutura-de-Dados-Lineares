6f16e2d37c3e7894cdbafa5cad1a23e08cc291c3
V-Matheus <victormatheus507@gmail.com>
2025-08-17 19:43:35 -0300

    fix: SelectSort

--- Sort/Sort.java
+++ Sort/Sort.java
@@ -27,15 +27,17 @@ public class Sort {
   public class SelectSort {
     public void sort(int[] list) {
       for(int i = 0; i < list.length - 1; i++) {
-        int minValue = list[i];
+        int minIndex = i;
 
         for(int j = i + 1; j < list.length; j++) {
-          if(list[j] < minValue) {
-            minValue = list[j];
-            list[j] = list[i];
-            list[i] = minValue;
+          if(list[j] < list[minIndex]) {
+            minIndex = j;
           }
         }
+
+        int temp = list[minIndex];
+        list[minIndex] = list[i];
+        list[i] = temp;
       }
     }
 
